9df00329e481f77b5faa1ea9ec71c3a37639ac95
Erica Sadun <erica@ericasadun.com>
2016-10-01 20:41:33 -0600

    Updated README with links to other tm utils

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Perform a time machine diff on text files
 Usage: tmdiff (--list)
        tmdiff [offset: 1] path
 ```
+
+#### See also [tmcp](https://github.com/erica/tmcp), [tmls](https://github.com/erica/tmls)
--- tmdiff/main.swift
+++ tmdiff/main.swift
@@ -12,29 +12,58 @@ public let manager = FileManager.default
 var arguments = CommandLine.arguments
 let appName = arguments.remove(at: 0).lastPathComponent
 func usage() -> Never {
-    print("Perform a time machine diff on text files")
-    print("Usage: \(appName) --list")
-    print("       \(appName) [offset: 1] path")
+    print("Perform a time machine diff on a text file")
+    print("       \(appName) --list (count)")
+    print("       \(appName) --help")
+    print("       \(appName) (--offset count) path")
     exit(-1)
 }
 
-// Separate arguments
-let options = arguments.filter({ $0.hasPrefix("--") })
-arguments = arguments.filter({ !$0.hasPrefix("--") })
+// Help message
+if arguments.contains("--help") { usage() }
 
 // Fetch time machine backup list in reverse order
-let tmitems = tmlist()
+let tmItems = tmlist()
 
-// Process options
-if options.contains("--list") {
-    tmitems.enumerated().forEach {
+// Perform Time Machine backup list
+if arguments.contains("--list") {
+    var max = tmItems.count
+    if let argOffset = arguments.index(of: "--list"),
+        arguments.index(after: argOffset) < arguments.endIndex
+    {
+        let countString = arguments[arguments.index(after: argOffset)]
+        if let count = Int(countString), count < max { max = count }
+    }
+    tmItems.prefix(upTo: max).enumerated().forEach {
         print("\($0.0): \($0.1.ns.lastPathComponent)")
     }
     exit(0)
 }
 
+// Process offset
+var offset = 1
+if arguments.contains("--offset") {
+    var max = tmItems.count
+    if let argOffset = arguments.index(of: "--offset"),
+        arguments.index(after: argOffset) < arguments.endIndex {
+        let countOffset = arguments.index(after: argOffset)
+        let countString = arguments[countOffset]
+        if let count = Int(countString), count < max { offset = count }
+        else { print("Offset invalid or too high (max is \(max - 1))"); exit(-1) }
+        [countOffset, argOffset].forEach { arguments.remove(at: $0) }
+    } else {
+        print("Invalid use of --offset (must be followed by a number)"); exit(-1)
+    }
+}
+
+// Filter arguments
+arguments = arguments.filter({ !$0.hasPrefix("--") })
+
 // Only valid way forward is `path` or `offset path`
-guard 1...2 ~= arguments.count else { usage() }
+guard arguments.count == 1 else {
+    print("Missing file argument")
+    usage()
+}
 
 // Fetch core path, and make canonical
 let corePath = arguments.last!
@@ -48,21 +77,10 @@ guard manager.fileExists(atPath: corePath, isDirectory: &isDir),
 }
 var path = components.joined(separator: "/")
 
-// Update offset if count parameter was supplied
-var offset = 1
-if arguments.count == 2, let offsetCount = Int(arguments[0]) {
-    offset = offsetCount
-}
-
-// Test offset
-guard tmitems.count > offset else {
-    print("Time machine offset too high (\(offset) vs \(tmitems.count)). Bailing")
-    exit(-1)
-}
-
 // Retrieve specific backup
-let tm_path = (tmitems[offset] as String).appendingPathComponent(path)
+let tmPath = (tmItems[offset] as String).appendingPathComponent(path)
 path = "/Volumes/" + path
 
 // Diff 'em and get out of Dodge
-print(diff(tm_path, path))
+print("Time Machine: \(tmItems[offset].lastPathComponent)\n")
+print(diff(tmPath, path))
